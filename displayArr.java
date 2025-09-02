bd7cfb1d4905651327d323cedc9c323efdeadf22
shiivvxm <shivamkumarrr437@gmail.com>
2025-09-03 00:13:53 +0530

    Add files via upload

---
+++ displayArr.java
@@ -0,0 +1,7 @@
+public class displayArr {
+    public static void main(String[] args) {
+        int[] numArr = ArrayUtility.inputArray();
+        System.out.println("array is");
+        ArrayUtility.displayArr(numArr);
+    }
+}
